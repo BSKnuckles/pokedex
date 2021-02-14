f7c102fa4c45881101a0a6dae2bd40885110ea31
Michael McNees <mcnees.michael@gmail.com>
2021-02-14 12:22:47 -0500

    Created basic component structure

---
+++ src/components/Search.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Search</h1>
+    <form>
+      <fieldset>
+        <div>
+          <label for="search">Search</label>
+          <input type="text" name="search" id="search" />
+        </div>
+      </fieldset>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+</style>
