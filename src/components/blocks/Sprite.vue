83530a96182d7b9b0a4b32fb6e2477dde2df7bb0
Michael McNees <mcnees.michael@gmail.com>
2021-02-27 20:20:21 -0500

    Created sprite component

---
+++ src/components/blocks/Sprite.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <img v-if="this.hasUrl" :src="sprite" :alt="sprite" />
+    <p v-if="!this.hasUrl">Not available</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sprite",
+  props: {
+    sprite: String,
+  },
+  computed: {
+    hasUrl: function () {
+      if (this.sprite == null) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+</style>
